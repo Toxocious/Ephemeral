f5490c1b0f0b09e7ce2ff750b15ca207d706afdb
Toxocious <toxocious@gmail.com>
2024-05-21 09:36:39 -0700

    fix: grid shader fragment and vertex code

--- Projects/Core/Assets/Shaders/Grid/grid.fragment.glsl
+++ Projects/Core/Assets/Shaders/Grid/grid.fragment.glsl
@@ -1,65 +1,67 @@
-#version 330 core
+#version 410 core
 
-/*
-Special thanks to the post (https://github.com/martin-pr/possumwood/wiki/Infinite-ground-plane-using-GLSL-shaders) author: Martin Prazak (http://www.mprazak.info/)
-and the post (http://asliceofrendering.com/scene%20helper/2020/01/05/InfiniteGrid/) author: Marie-Eve Dubé (https://github.com/bugztroll)
-*/
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+  precision highp float;
+#else
+  precision mediump float;
+#endif
 
-in vec3 nearPoint;
-in vec3 farPoint;
+layout(location = 0) out vec4 o_color;
 
+in vec3 near;
+in vec3 far;
+
+uniform float[2] u_nearfar;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec4 FragColor;
+vec4 grid(vec3 point, float scale, bool is_axis) {
+    vec2 coord = point.xz * scale;
+    vec2 dd    = fwidth(coord);
+    vec2 uv    = fract(coord - 0.5) - 0.5;
+    vec2 grid  = abs(uv) / dd;  // TODO: figure this out, adjust the line thickness
+    float line = min(grid.x, grid.y);
+    float min_z = min(dd.y, 1.0);
+    float min_x = min(dd.x, 1.0);
+    vec4 col    = vec4(0.3);
+    col.a       = 1.0 - min(line, 1.0);
 
-vec4 grid( vec3 fragPos3D, float scale )
-{
-    vec2 coord = fragPos3D.xz * scale;
-    vec2 derivative = fwidth(coord);
-    vec2 grid = abs(fract(coord - 0.5) - 0.5) / derivative;
-
-    if (grid.x > 0.999 && grid.y > 0.999)
-    {
-        discard;
-    }
-    else
-    {
-        vec3 mainaxis = vec3(0.0, 0.0, 0.0);
-
-        // z axis
-        if ( fragPos3D.x > -0.02 && fragPos3D.x < 0.02 )
-            mainaxis = vec3(0.0, 0.0, 1.0);
-
-        // x axis
-        if ( fragPos3D.z > -0.02 && fragPos3D.z < 0.02 )
-            mainaxis = vec3(1.0, 0.0, 0.0);
-
-        return vec4(mainaxis.xyz, 1.0);
-    }
+    if (-1.0 * min_x < point.x && point.x < 0.1 * min_x && is_axis)
+        col.rgb = vec3(0.427, 0.792, 0.909);
+    if (-1.0 * min_z < point.z && point.z < 0.1 * min_z && is_axis)
+        col.rgb = vec3(0.984, 0.380, 0.490);
+    return col;
 }
 
-float computeDepth( vec3 pos )
-{
-    vec4 clip_space_pos = projection * view * vec4(pos.xyz, 1.0);
-
-    float clip_space_depth = clip_space_pos.z / clip_space_pos.w;
-
-	float far = gl_DepthRange.far;
-	float near = gl_DepthRange.near;
-
-	float depth = (((far-near) * clip_space_depth) + near + far) / 2.0;
-
-	return depth;
+float compute_depth(vec3 point) {
+    vec4 clip_space = projection * view * vec4(point, 1.0);
+    float clip_space_depth = clip_space.z / clip_space.w;
+    float far  = gl_DepthRange.far;
+    float near = gl_DepthRange.near;
+    float depth = (((far - near) * clip_space_depth) + near + far) / 2.0;
+    return depth;
 }
 
-void main()
-{
-    float t = -nearPoint.y / (farPoint.y - nearPoint.y);
+float compute_fade(vec3 point) {
+    vec4 clip_space = projection * view * vec4(point, 1.0);
+    float clip_space_depth = (clip_space.z / clip_space.w) * 2.0 - 1.0;
+    float near = u_nearfar[0];
+    float far  = u_nearfar[1];
+    float linear_depth = (2.0 * near * far) / (far + near - clip_space_depth * (far - near));
+    return linear_depth / far;
+}
 
-    vec3 fragPos3D = nearPoint + t * (farPoint - nearPoint);
+void main() {
+    float t = -near.y / (far.y - near.y);
+    vec3  R =  near + t * (far - near);
+    float is_on = float(t > 0);
 
-    gl_FragDepth = computeDepth(fragPos3D);
+    //float fade = max(0, 1.0 - compute_fade(R));
+    float fade = smoothstep(0.04, 0.0, compute_fade(R));
+    o_color = grid(R, 1, true);
+    //o_color += grid(R, 10, false) * 0.25;
+    o_color *= fade;
 
-    FragColor = (grid(fragPos3D, 1))* float(t > 0);
+    o_color *= is_on;
+    gl_FragDepth = compute_depth(R);
 }
--- Projects/Core/Assets/Shaders/Grid/grid.vertex.glsl
+++ Projects/Core/Assets/Shaders/Grid/grid.vertex.glsl
@@ -1,34 +1,29 @@
-#version 330 core
+#version 410 core
 
-/*
-Special thanks to the post (https://github.com/martin-pr/possumwood/wiki/Infinite-ground-plane-using-GLSL-shaders) author: Martin Prazak (http://www.mprazak.info/)
-and the post (http://asliceofrendering.com/scene%20helper/2020/01/05/InfiniteGrid/) author: Marie-Eve Dubé (https://github.com/bugztroll)
-*/
+layout (location = 0) in vec2 a_position;
 
-vec3 gridPlane[6] = vec3[] (
-    vec3(1, 1, 0), vec3(-1, -1, 0), vec3(-1, 1, 0),
-    vec3(-1, -1, 0), vec3(1, 1, 0), vec3(1, -1, 0)
-);
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+  precision highp float;
+#else
+  precision mediump float;
+#endif
+
+layout(location = 0) out vec4 o_color;
+out vec3 near;
+out vec3 far;
 
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 nearPoint;
-out vec3 farPoint;
-
-vec3 UnprojectPoint( float x, float y, float z, mat4 viewmat, mat4 proj )
-{
-    mat4 viewInv = inverse(viewmat);
-    mat4 projInv = inverse(proj);
-    vec4 unprojectedPoint =  viewInv * projInv * vec4(x, y, z, 1.0);
-
-    return unprojectedPoint.xyz / unprojectedPoint.w;
+vec3 unproject_point(float x, float y, float z) {
+    mat4 inv = inverse(projection * view);
+    vec4 unproj_point = inv * vec4(x, y, z, 1.f);
+    return unproj_point.xyz / unproj_point.w;
 }
 
-void main()
-{
-   vec3 p = gridPlane[gl_VertexID];
-   nearPoint = UnprojectPoint(p.x, p.y, 0.0, view, projection).xyz; // unprojecting on the near plane
-   farPoint = UnprojectPoint(p.x, p.y, 1.0, view, projection).xyz; // unprojecting on the far plane
-   gl_Position = vec4(p, 1.0);
+void main() {
+    vec2 p = a_position;
+    near = unproject_point(p.x, p.y, -1.f);
+    far  = unproject_point(p.x, p.y,  1.f);
+    gl_Position = vec4(a_position, 0.0f, 1.0f);
 }
